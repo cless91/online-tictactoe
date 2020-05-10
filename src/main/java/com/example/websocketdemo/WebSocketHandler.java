213f2828aaec6b97114dff2ca2eafcff897189e8
joseph m'bimbi-bene <Joseph.Mbimbi-Bene.ext@ing.com>
2020-05-10 15:40:28 +0200

    first commit

---
+++ src/main/java/com/example/websocketdemo/WebSocketHandler.java
@@ -0,0 +1,48 @@
+package com.example.websocketdemo;
+
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebSocketHandler extends AbstractWebSocketHandler {
+
+    List<WebSocketSession> sessions = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        super.afterConnectionClosed(session, status);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.printf("txt msg received: %s\n", message.getPayload());
+        TextMessage textMessage = new TextMessage(String.format("%s: %s", session.getId(), message.getPayload()));
+        for (WebSocketSession webSocketSession : sessions) {
+            if (!webSocketSession.equals(session)) {
+                webSocketSession.sendMessage(textMessage);
+            }
+        }
+    }
+
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws Exception {
+        System.out.println("binary msg received");
+        for (WebSocketSession webSocketSession : sessions) {
+            if (!webSocketSession.equals(session)) {
+                webSocketSession.sendMessage(message);
+            }
+        }
+    }
+}
---
+++ src/test/java/com/example/websocketdemo/WebsocketDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.websocketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+@WebAppConfiguration
+//@ContextConfiguration(classes = WebSocketConfiguration.class)
+class WebsocketDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
