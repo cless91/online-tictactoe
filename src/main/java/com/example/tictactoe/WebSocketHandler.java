0c20b464c7d72dfccb6172765203f57214a28a49
joseph m'bimbi-bene <Joseph.Mbimbi-Bene.ext@ing.com>
2020-05-14 05:59:49 +0200

    wip front

--- src/main/java/com/example/tictactoe/WebSocketHandler.java
+++ src/main/java/com/example/tictactoe/WebSocketHandler.java
@@ -4,17 +4,24 @@ import com.example.tictactoe.game.Game;
 import com.example.tictactoe.game.GameApplication;
 import com.example.tictactoe.game.Player;
 import com.example.tictactoe.presentation.GamePresentation;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+@RestController
 public class WebSocketHandler extends AbstractWebSocketHandler {
 
     List<WebSocketSession> sessions = new ArrayList<>();
@@ -35,7 +42,10 @@ public class WebSocketHandler extends AbstractWebSocketHandler {
         List<GamePresentation> gamePresentations = gameApplication.listGames().stream()
                 .map(this::toGamePresentation)
                 .collect(Collectors.toList());
-        session.sendMessage(new TextMessage(objectMapper.writeValueAsString(gamePresentations)));
+        Map<String,Object> data = new HashMap<>();
+        data.put("sessionId",session.getId());
+        data.put("currentGames",gamePresentations);
+        session.sendMessage(new TextMessage(objectMapper.writeValueAsString(data)));
         super.afterConnectionEstablished(session);
     }
 
@@ -72,6 +82,17 @@ public class WebSocketHandler extends AbstractWebSocketHandler {
         for (WebSocketSession webSocketSession : sessions) {
             if (!webSocketSession.equals(session)) {
                 webSocketSession.sendMessage(message);
+            }
+        }
+    }
+
+    @PostMapping("createGame")
+    public void createGame(String playerId) throws IOException {
+        Game newGame = gameApplication.createNewGame(new Player(playerId));
+        GamePresentation newGamePresentation = toGamePresentation(newGame);
+        for (WebSocketSession webSocketSession : sessions) {
+            if (!webSocketSession.getId().equals(playerId)) {
+                webSocketSession.sendMessage(new TextMessage(objectMapper.writeValueAsString(newGamePresentation)));
             }
         }
     }
